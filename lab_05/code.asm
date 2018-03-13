b2a6753f67ee406391fe9824803a06563751a318
wh75er <bokseng12@gmail.com>
2018-03-13 17:43:44 +0300

    bubble sort without optimization

--- lab_05/code.asm
+++ lab_05/code.asm
@@ -5,8 +5,8 @@ TITLE lab_02
          SSTACK     ENDS
 
          DSEG          SEGMENT  PARA PUBLIC 'DATA'
-        x         	DW      1,2,3,4,5
-		nmax		DW		5
+        x         	DW      1,2,3,4,5,8,3,9
+		nmax		DW		8
          DSEG          ENDS
 
  SUBTTL MAIN PROGRAM
@@ -16,8 +16,37 @@ TITLE lab_02
 _begin:
 	mov ax,DSEG
 	mov ds,ax
+	lea bx,x
+	dec nmax
+	mov cx,nmax
+	MainLoop:
+		mov di,0
+		mov si,2
+		mov dx,cx
+		mov cx,nmax
+		InsideLoop:
+			mov ax,[bx][si]
+			cmp [bx][di],ax
+			jle _go_forward
+			jg _switch
 
-	int 20h
+
+			_switch: 
+				xchg ax,[bx][di]
+				xchg ax,[bx][si]
+				xchg ax,[bx][di]
+
+			_go_forward:
+				add di,2
+				add si,2
+				loop InsideLoop
+
+		mov cx,dx
+		loop MainLoop
+
+
+	mov ah,4ch
+	int 21h
 		
 
 CSEG      ENDS
