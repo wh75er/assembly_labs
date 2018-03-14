17fa7740698f7a5ea468031d2c1ba8baa804c5e7
wh75er <bokseng12@gmail.com>
2018-03-14 09:45:39 +0300

    last version of program

--- lab_05/code.asm
+++ lab_05/code.asm
@@ -6,7 +6,6 @@ TITLE lab_02
 
          DSEG          SEGMENT  PARA PUBLIC 'DATA'
         x         	DW      1,4,3,4,5,9,3,8
-		nmax		DW		8
          DSEG          ENDS
 
  SUBTTL MAIN PROGRAM
@@ -17,16 +16,11 @@ _begin:
 	mov ax,DSEG
 	mov ds,ax
 	lea bx,x
-	dec nmax
-	mov cx,nmax
+	mov cx,7
 	MainLoop:
 		mov di,0
 		mov si,2
 		mov dx,cx
-		mov ax,nmax
-		sub ax,cx
-		mov cx,nmax
-		sub cx,ax
 		InsideLoop:
 			mov ax,[bx][si]
 			cmp [bx][di],ax
